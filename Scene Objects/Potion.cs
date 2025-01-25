6c2349669d5d37aa4fc732ff9d3813673019096f
Cynichill <logancampbell@live.co.uk>
2025-01-25 22:24:23 +0000

    Potion ingredient array added

--- Scene Objects/Potion.cs
+++ Scene Objects/Potion.cs
@@ -6,8 +6,9 @@ public partial class Potion : Node2D
 	BarMaster barMaster;
 	RichTextLabel score;
 	Timer timer;
+	public Godot.Collections.Array<Ingredient> _ingredientArray;
 
-	bool timeEnded = false;
+    bool timeEnded = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -25,6 +26,20 @@ public partial class Potion : Node2D
 			score.Text = barMaster.CalculateOverallScore().ToString();
 		}
 	}
+
+	private void AddIngredient(Ingredient ing)
+	{
+		_ingredientArray.Add(ing);
+		barMaster.AddNaturalRate(ing.properties.baseStats.X, BarMaster.BarType.Stability);
+        barMaster.AddNaturalRate(ing.properties.impulseStats.X, BarMaster.BarType.Stability);
+        barMaster.AddNaturalRate(ing.properties.dynamicStats.X, BarMaster.BarType.Stability);
+        barMaster.AddNaturalRate(ing.properties.baseStats.Y, BarMaster.BarType.Potency);
+        barMaster.AddNaturalRate(ing.properties.impulseStats.Y, BarMaster.BarType.Potency);
+        barMaster.AddNaturalRate(ing.properties.dynamicStats.Y, BarMaster.BarType.Potency);
+        barMaster.AddNaturalRate(ing.properties.baseStats.Z, BarMaster.BarType.Bubbliness);
+        barMaster.AddNaturalRate(ing.properties.impulseStats.Z, BarMaster.BarType.Bubbliness);
+        barMaster.AddNaturalRate(ing.properties.dynamicStats.Z, BarMaster.BarType.Bubbliness);
+    }
 
 	private void _on_timer_timeout()
 	{
