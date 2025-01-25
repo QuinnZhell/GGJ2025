e7102b9c4e3ea89f594eda5f3ddd829c29b6b9bd
NadesOnGit <45142809+NadesOnGit@users.noreply.github.com>
2025-01-26 00:40:20 +0100

    Add 'spit' minigame

--- brewsys/spit/Spit.cs
+++ brewsys/spit/Spit.cs
@@ -10,6 +10,8 @@ public partial class Spit : Node2D
 
 	// Time limit for the quick time event (in seconds)
 	private float qteTimeLimit = 2.0f;
+	private float qteSweetSpot = 1.5f;
+	private float sweetSpotBuffer = 0.25f;
 
 	public override void _Ready()
 	{
@@ -21,13 +23,8 @@ public partial class Spit : Node2D
 		AddChild(qteTimer);
 	}
 
-	public async Task WaitForQuarterSecond()
-	{
-		await Task.Delay(20);
-	}
-
 	// Start the QTE when called
-	public async void StartQTE()
+	public void StartQTE()
 	{
 		if (!qteStarted)
 		{
@@ -37,28 +34,31 @@ public partial class Spit : Node2D
 			// Start the timer
 			qteTimer.Start();
 
-            await WaitForQuarterSecond();
-
-            // Show some visual feedback (e.g., prompt or UI indicator)
-            GD.Print("Quick Time Event Started! Press 'Space' to pour.");
+			// Show some visual feedback (e.g., prompt or UI indicator)
+			GD.Print("Quick Time Event Started! Press 'E' to pour.");
 		}
 	}
 
 	public override void _Process(double delta)
 	{
 		// If user presses space E, start spit QTE
-		if (!qteStarted && Input.IsActionJustPressed("spit")) 
+		if (!qteStarted && Input.IsActionJustPressed("spit"))
 		{
 			StartQTE();
 		}
 
 		// Check if the player pressed the right key (e.g., "Space") during the QTE
-		if (qteStarted && Input.IsActionJustPressed("spit")) // "ui_accept" is typically mapped to 'Space' key
+		if (InSweetSpot(qteTimeLimit - qteTimer.TimeLeft) && qteStarted && Input.IsActionJustReleased("spit")) // "ui_accept" is typically mapped to 'Space' key
 		{
 			qteSuccess = true;
 			qteTimer.Stop(); // Stop the timer as the player succeeded
 			HandleQteResult();
 		}
+	}
+
+	private bool InSweetSpot(double sweetSpot) 
+	{
+		return (sweetSpot >= qteSweetSpot - sweetSpotBuffer && sweetSpot <= qteSweetSpot + sweetSpotBuffer);
 	}
 
 	private void OnQteTimeout()
